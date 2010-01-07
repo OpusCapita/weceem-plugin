852a4dd361424ca171813297511d9a7af1d14ba6
Marc Palmer <marc@anyware.co.uk>
2010-01-07 15:10:44 +0000

    Added ant dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.dependency.resolution = {
     inherits "global" // inherit Grails default dependencies
     dependencies {
-        compile "commons-codec:commons-codec:1.3"
+        runtime "commons-codec:commons-codec:1.3"
+        runtime 'org.apache.ant:ant:1.7.1'
     }
 }
