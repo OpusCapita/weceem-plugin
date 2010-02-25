cc3480b4dc4e6e057bde4bd8a2cf04dc45bc8b14
Marc Palmer <marc@anyware.co.uk>
2010-02-25 16:50:41 +0000

    Upgraded to grails 1.2.1 and added grails 1.2 ivy dependency resolution

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,8 +2,15 @@ grails.project.dependency.resolution = {
     inherits "global" // inherit Grails default dependencies
     
     dependencies {
-        //compile "commons-codec:commons-codec:1.3"
+        flatDir dirs:"lib" // need this for textilej
+        
+        // Workarounds for Grails 1.2 not shipping ant in WAR
         compile 'org.apache.ant:ant:1.7.1'
         compile 'org.apache.ant:ant-launcher:1.7.1'
+
+        // Our specific dependencies
+        compile 'com.lowagie:iText:2.1.3'
+        compile 'net.java.textilej:net.java.textilej:2.2.854'
+        compile 'xstream:xstream:1.2.1'
     }
 }
