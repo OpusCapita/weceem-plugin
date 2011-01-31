84b4a2db6e19de539f5b8f0783fff314f87092c2
Marc Palmer <marc@anyware.co.uk>
2011-01-31 11:57:27 +0000

    More events docs

--- src/groovy/org/weceem/event/WeceemEvents.groovy
+++ src/groovy/org/weceem/event/WeceemEvents.groovy
@@ -4,13 +4,18 @@ import org.weceem.content.WcmContent
 
 class WeceemEvents {
     static events = {
+        /* Called after a new node has been created */
         contentDidGetCreated { WcmContent node -> }
 
+        /* Called before a node is deleted */
         contentWillBeDeleted { WcmContent node -> }
+        /* Called after a node is deleted */
         contentDidGetDeleted { WcmContent node -> }
         
+        /* Called after a new node is updated */
         contentDidGetUpdated { WcmContent node -> }
         
+        /* Called after a new node is moved to a new parent */
         contentDidMove { WcmContent node -> }
     }
     
