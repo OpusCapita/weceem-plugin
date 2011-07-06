93d49d095c6e395164dcf6f988e0c5d85269be94
Marc Palmer <marc@anyware.co.uk>
2011-07-06 11:25:22 +0000

    Fix for upload issue

--- src/groovy/org/weceem/filter/UploadedFileFilter.groovy
+++ src/groovy/org/weceem/filter/UploadedFileFilter.groovy
@@ -15,6 +15,7 @@ class UploadedFileFilter implements Filter {
     def wcmContentRepositoryService
     def wcmSecurityService
     def cacheHeadersService
+    def uploadURI
     
     void init(FilterConfig config) throws ServletException {
         def applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.servletContext)
@@ -22,6 +23,7 @@ class UploadedFileFilter implements Filter {
         wcmSecurityService = applicationContext.wcmSecurityService
         cacheHeadersService = applicationContext.cacheHeadersService
         wcmContentFingerprintService = applicationContext.wcmContentFingerprintService
+        uploadURI = wcmContentRepositoryService.uploadUrl
     }
 
     void destroy() {
@@ -30,74 +32,76 @@ class UploadedFileFilter implements Filter {
     void doFilter(ServletRequest request, ServletResponse response,
         FilterChain chain) throws IOException, ServletException {
 
-        // Force a continuous hib session across service calls to avoid detached objects causing lazy init problems
-        WcmContent.withTransaction {
-            def info = wcmContentRepositoryService.resolveSpaceAndURIOfUploadedFile(request.requestURI.decodeURL() - request.contextPath)
-            def space = info.space
-            def uri = info.uri
+        def ruri = request.requestURI[request.contextPath.size()..-1]
+        if (ruri.startsWith(uploadURI)) {
+            // Force a continuous hib session across service calls to avoid detached objects causing lazy init problems
+            WcmContent.withTransaction {
+                def info = wcmContentRepositoryService.resolveSpaceAndURIOfUploadedFile(request.requestURI.decodeURL() - request.contextPath)
+                def space = info.space
+                def uri = info.uri
  
-            def canView = wcmSecurityService.hasPermissions(space, uri, [WeceemSecurityPolicy.PERMISSION_VIEW])
-            if (!canView) {
-                canView = wcmSecurityService.hasPermissions(space, uri, [WeceemSecurityPolicy.PERMISSION_EDIT])
-            }
+                def canView = wcmSecurityService.hasPermissions(space, uri, [WeceemSecurityPolicy.PERMISSION_VIEW])
+                if (!canView) {
+                    canView = wcmSecurityService.hasPermissions(space, uri, [WeceemSecurityPolicy.PERMISSION_EDIT])
+                }
         
-            // @todo also implement Draft status checks here so ppl cannot view drafts even if they have perms (e.g. guests)
-            if (!canView) {
-                response.sendError(503, "You do not have permission to view this resource")
-                chain.doFilter(request, response)
-                return
-            }
+                // @todo also implement Draft status checks here so ppl cannot view drafts even if they have perms (e.g. guests)
+                if (!canView) {
+                    response.sendError(503, "You do not have permission to view this resource")
+                    chain.doFilter(request, response)
+                    return
+                }
 
-            def contentInfo = wcmContentRepositoryService.findContentForPath(uri, space)
-            def content
-            if (contentInfo?.content) {
-                content = contentInfo.content
-            }
+                def contentInfo = wcmContentRepositoryService.findContentForPath(uri, space)
+                def content
+                if (contentInfo?.content) {
+                    content = contentInfo.content
+                }
         
-            def f = wcmContentRepositoryService.getUploadPath(space, uri)
-            if (!f.exists()) {
-                response.sendError(404)
-                return
-            }
+                def f = wcmContentRepositoryService.getUploadPath(space, uri)
+                if (!f.exists()) {
+                    response.sendError(404)
+                    return
+                }
         
-            def ctx = [
-                cacheHeadersService:cacheHeadersService, 
-                wcmContentFingerprintService:wcmContentFingerprintService, 
-                wcmContentRepositoryService:wcmContentRepositoryService, 
-                request:request, 
-                response:response,
-            ]
-            cacheHeadersService.withCacheHeaders(ctx) { 
-                if (content) {
-                    etag {
-                        tagValue = wcmContentFingerprintService.getFingerprintFor(content)
+                def ctx = [
+                    cacheHeadersService:cacheHeadersService, 
+                    wcmContentFingerprintService:wcmContentFingerprintService, 
+                    wcmContentRepositoryService:wcmContentRepositoryService, 
+                    request:request, 
+                    response:response,
+                ]
+                cacheHeadersService.withCacheHeaders(ctx) { 
+                    if (content) {
+                        etag {
+                            tagValue = wcmContentFingerprintService.getFingerprintFor(content)
+                        }
                     }
-                }
             
-                lastModified {
-                    content ? wcmContentRepositoryService.getLastModifiedDateFor(content) : new Date(f.lastModified())
-                }
+                    lastModified {
+                        content ? wcmContentRepositoryService.getLastModifiedDateFor(content) : new Date(f.lastModified())
+                    }
             
-                generate {
-                    def cacheMaxAge = content?.validFor ?: 1
+                    generate {
+                        def cacheMaxAge = content?.validFor ?: 1
                     
-                    def publiclyCacheable = true
+                        def publiclyCacheable = true
                 
-                    cacheHeadersService.cache(response, [validFor: cacheMaxAge, shared:publiclyCacheable])
+                        cacheHeadersService.cache(response, [validFor: cacheMaxAge, shared:publiclyCacheable])
 
-                    def mt = content ? content.mimeType : MimeUtils.getDefaultMimeType(f.name)
+                        def mt = content ? content.mimeType : MimeUtils.getDefaultMimeType(f.name)
 
-                    response.setContentType(mt)    
-                    try {
-                        // @todo is this fast enough?    
-                        response.outputStream << f.newInputStream()
-                    } catch (IOException ioe) {
-                        // Munch. We should never do this but the client has gone away so...
+                        response.setContentType(mt)    
+                        try {
+                            // @todo is this fast enough?    
+                            response.outputStream << f.newInputStream()
+                        } catch (IOException ioe) {
+                            // Munch. We should never do this but the client has gone away so...
+                        }
                     }
                 }
             }
         }
- 
         chain.doFilter(request, response)
     }
 }
