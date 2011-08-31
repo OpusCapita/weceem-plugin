457db5ffd14db30af006634a036c7ea91cfb3d72
Marc Palmer <marc@anyware.co.uk>
2011-08-31 13:02:00 +0000

    Changed version to 1.1

--- WeceemGrailsPlugin.groovy
+++ WeceemGrailsPlugin.groovy
@@ -9,7 +9,7 @@ class WeceemGrailsPlugin {
     def _log = LogFactory.getLog('org.weceem.WeceemGrailsPlugin')
 
     // the plugin version
-    def version = "1.0.1.BUILD-SNAPSHOT"
+    def version = "1.1.BUILD-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.2 > *"
     
