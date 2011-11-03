39fd90295cc4d53c90e9f36bcf0e01b0b721ef36
Marc Palmer <marc@anyware.co.uk>
2011-11-03 17:34:37 +0000

    Updated dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,20 +24,17 @@ grails.project.dependency.resolution = {
     }
 
 	plugins { 
-	    /* 
-	    
-	    No support for > * so forget it, this kills ppl using newer versions
-	    
-		runtime ':searchable:0.5.5'
-		runtime ':navigation:1.1.1'
-		runtime ':bean-fields:1.0-RC3'
-		runtime ':jquery-ui:1.8.2.4'
-		runtime ':quartz:0.4.2'
-		runtime ':feeds:1.5'
-		runtime ':taggable:0.6.2'
-		runtime ':ckeditor:3.4.0'
-		runtime ':blueprint:0.9.1.1'
-		*/
-	}
+        compile ":searchable:0.6.3" 
+        compile ":quartz:0.4.2"
+        compile ":navigation:1.3.2"
+        compile ":ckeditor:3.5.2.0"
+        compile ":feeds:1.5"
+        compile ":bean-fields:1.0.RC5"
+        compile ":blueprint:0.9.1.1"
+        compile ":jquery:1.4.4.1"
+        compile ":jquery-ui:1.8.6.1"
+        compile ":cache-headers:1.1.5"
+        compile ":taggable:1.0"
+ 	}
 }
 
