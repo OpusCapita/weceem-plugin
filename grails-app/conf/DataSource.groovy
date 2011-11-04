b90adf381e0b1785f58a215b812ea5589f5e21a8
Marc Palmer <marc@anyware.co.uk>
2011-11-04 17:30:10 +0000

    Updated 2.0 code, 1.1.1.BUILD-SNAPSHOT

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,6 +1,6 @@
 dataSource {
 	pooled = true
-	driverClassName = "org.hsqldb.jdbcDriver"
+	driverClassName = "org.h2.Driver"
 	username = "sa"
 	password = ""
 }
@@ -14,7 +14,7 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:hsqldb:mem:devDB"
+			url = "jdbc:h2:mem:devDb;MVCC=TRUE"
 		}
 	}
 	test {
