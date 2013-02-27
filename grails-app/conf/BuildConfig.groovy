446d99207f124f271ef2f6e8db0999879054e1bc
Oliver Mihatsch <oliver.mihatsch@panoratio.com>
2013-02-27 20:31:44 +0100

    release plugin version upgrade ; newest version of other dependencies in comments for later upgrade

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,26 +24,26 @@ grails.project.dependency.resolution = {
     }
 
 	plugins { 
-        compile ":bean-fields:1.0.RC5"
-        compile ":blueprint:0.9.1.1"
+        compile ":bean-fields:1.0.RC5" //newest: 1.0; replacement fields-plugin
+        compile ":blueprint:0.9.1.1" // newest: 1.0.2
         compile ":cache-headers:1.1.5"
-        compile ":ckeditor:3.6.0.0"
+        compile ":ckeditor:3.6.0.0" // newest: 3.6.3.0
         compile ":feeds:1.5"
-        test(":functional-test:1.3-RC1") { 
+        test(":functional-test:1.3-RC1") { // neweset : 2.0.RC1
             export = false 
         }
         compile ":jquery:1.8.3"
         compile ":jquery-ui:1.8.24"
         compile ":navigation:1.3.2"
-        compile ":quartz:0.4.2"
-        compile ":searchable:0.6.3" 
-        compile ":taggable:1.0"
+        compile ":quartz:0.4.2" // newest : 1.0-RC5
+        compile ":searchable:0.6.4" 
+        compile ":taggable:1.0.1"
 
         // For serlvet filter ordering
         provided ":webxml:1.4.1"
 
         compile ":hibernate:$grailsVersion"
-        compile(":release:1.0.0.RC3") {
+        compile(":release:2.2.1") {
             export = false
         }
         build(":tomcat:$grailsVersion") {
