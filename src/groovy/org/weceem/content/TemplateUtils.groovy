347c22f2217223bd2dfa8194f48172f328f1e391
July Antonicheva <july.antonicheva@jcatalog.com>
2013-12-13 22:47:44 +0300

    (WCM-811) Updating template getting for content

--- src/groovy/org/weceem/content/TemplateUtils.groovy
+++ src/groovy/org/weceem/content/TemplateUtils.groovy
@@ -3,7 +3,9 @@ package org.weceem.content
 class TemplateUtils {
     static getTemplateForContent(WcmContent content) {
         def template = content.metaClass.hasProperty(content, 'template') ? content.template : null
-        if ((template == null) && (content.parent != null)) {
+        if ((template == null)
+            && (content.metaClass.hasProperty(content, 'template'))
+            && (content.parent != null)) {
             return getTemplateForContent(content.parent)
         } else {
             return template
