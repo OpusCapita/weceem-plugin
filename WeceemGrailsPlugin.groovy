a104bf2859dfc8ee5980e0bd5b86b3a30232496e
Alexey Sergeev <alexey.sergeev@jcatalog.com>
2014-01-16 08:16:02 +0300

    Releasing 1.2-M1 version

--- WeceemGrailsPlugin.groovy
+++ WeceemGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class WeceemGrailsPlugin {
     def _log = LogFactory.getLog('org.weceem.WeceemGrailsPlugin')
 
     // the plugin version
-    def version = "1.2-SNAPSHOT"
+    def version = "1.2-M1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
 
