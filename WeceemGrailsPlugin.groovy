1d0cb9033d7f86450fce51c9a39393f212c3517a
July Antonicheva <july.antonicheva@jcatalog.com>
2014-10-15 23:23:14 +0300

    (WCM-899) Preparing to release

--- WeceemGrailsPlugin.groovy
+++ WeceemGrailsPlugin.groovy
@@ -10,7 +10,7 @@ class WeceemGrailsPlugin {
     def _log = LogFactory.getLog('org.weceem.WeceemGrailsPlugin')
 
     // the plugin version
-    def version = "1.3"
+    def version = "1.3-M1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
 
